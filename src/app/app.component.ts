05ee8c0ac83fb354e0454fc21c68fe7b1e0eea96
bhoomi-12 <bhoomo2711@gmail.com>
2022-06-25 13:29:57 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,26 +9,10 @@ export class AppComponent {
   name = 'Angular ' + VERSION.major;
   // myClass:string;
   tabSelected: string;
-  tbbi: Array<string> = [];
-  tbtri: Array<string> = [];
+  tbbi = [];
+  tbtri= [];
   names = [
-    'Muneshsir',
-    'Zalak',
-    'Grishma',
-    'Vishal',
-    'Neel',
-    'Harsh',
-    'Akash',
-    'Prachi',
-    'peenal',
-    'peenal',
-    'Bhoomi',
-    'Pooja',
-    'Dipesh',
-    'Roger',
-    'Jatin',
-    'Peenal',
-    'Dhrishil',
+    'Muneshsir','Zalak','Grishma','Vishal','Neel','Harsh','Akash','Prachi','peenal','Bhoomi','Pooja','Dipesh','Roger', 'Jatin','Peenal', 'Dhrishil',
   ];
   getNames(name) {
     if (name == 'Bhoomi' || name == 'Muneshsir' || name == 'Zalak')
@@ -38,12 +22,19 @@ export class AppComponent {
   remove(name) {
       this.names.splice(this.names.indexOf(name), 1);
   }
-  addToTab(xyz){
-    this.names.push(xyz);
+  addTab(name){
+    this.tbbi.push(this.name);
   }
-  addTriTab(itm){
-
+  removebi(tbtwo) {
+    this.tbbi.splice(this.tbbi.indexOf(tbtwo), 1);
+}
+addbiTab(name){
+  this.tbtri.push(this.name);
+}
+removetri(tbthree){
+  this.tbtri.splice(this.tbtri.indexOf(tbthree),1)
+}
+addtriTab(name){
+  this.tbbi.push(this.name);
   }
 }
-
-
