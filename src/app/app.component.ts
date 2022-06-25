6b1066d182c16666a8a72d294458798da11555bd
bhoomi-12 <bhoomo2711@gmail.com>
2022-06-25 06:45:29 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,47 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
   // myClass:string;
-  tabSelected:string;
-  names=["Muneshsir","Zalak" ,"Grishma" , "Vishal" , "Neel" , "Harsh" , "Akash" ,"Prachi" ,"peenal" , "peenal","Bhoomi" ,"Pooja" , "Dipesh" , "Roger" , "Jatin" , "Peenal" , "Dhrishil"];
-  getNames(name){
-    if(name== "Bhoomi" || name=="Muneshsir")
-    return 'mee';
-    else return "batchmates"
+  tabSelected: string;
+  tbbi: Array<string> = [];
+  tbtri: Array<string> = [];
+  names = [
+    'Muneshsir',
+    'Zalak',
+    'Grishma',
+    'Vishal',
+    'Neel',
+    'Harsh',
+    'Akash',
+    'Prachi',
+    'peenal',
+    'peenal',
+    'Bhoomi',
+    'Pooja',
+    'Dipesh',
+    'Roger',
+    'Jatin',
+    'Peenal',
+    'Dhrishil',
+  ];
+  getNames(name) {
+    if (name == 'Bhoomi' || name == 'Muneshsir' || name == 'Zalak')
+      return 'mee';
+    else return 'batchmates';
+  }
+  remove(name) {
+      this.names.splice(this.names.indexOf(name), 1);
+  }
+  addToTab(xyz){
+    this.names.push(xyz);
+  }
+  addTriTab(itm){
+
   }
 }
+
+
